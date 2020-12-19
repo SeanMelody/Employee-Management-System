a3a7efa0af426e46cd58b4fa997deade0b2631ea
Sean Melody <seandmelody@gmail.com>
2020-12-18 17:23:09 -0800

    More View Options Added

--- app.js
+++ app.js
@@ -18,20 +18,25 @@ connection.connect((err) => {
     start();
 });
 
+// Start the program!
 function start() {
+    // Prompt the first question
     inquirer
+        // Options are Add, View, Update or Exit the Program
         .prompt({
             name: 'addViewUpdate',
             type: 'list',
             message: 'Would you like to Add, View or Update Employee Roles?',
             choices: ['Add', 'View', 'Update', 'Exit'],
         })
+        // Get the answer to the question
         .then((answer) => {
-            // based on their answer, either call the bid or the post functions
+            // based on their answer, either call the Add, View, Update, or Exit functions!
             if (answer.addViewUpdate === 'Add') {
                 console.log("Add")
             } else if (answer.addViewUpdate === 'View') {
-                viewEmployees();
+                departmentRoleEmployee();
+                // viewEmployees();
             } else if (answer.addViewUpdate === 'Update') {
                 console.log("Update")
             } else {
@@ -42,9 +47,60 @@ function start() {
         });
 };
 
+
+function departmentRoleEmployee() {
+    inquirer
+        .prompt({
+            name: 'deptRoleEmp',
+            type: 'list',
+            message: 'Would you like to View Departments, Roles or Employees?',
+            choices: ['Departments', 'Roles', 'Employees', 'Main Menu'],
+        })
+
+        // Get the answer to the question
+        .then((answer) => {
+            // based on their answer, either call the Add, View, Update, or Exit functions!
+            if (answer.deptRoleEmp === 'Departments') {
+                console.log("Departments")
+            } else if (answer.deptRoleEmp === 'Roles') {
+                console.log("Roles")
+                // viewEmployees();
+            } else if (answer.deptRoleEmp === 'Employees') {
+                console.log("Employees")
+            } else {
+                start()
+            }
+        });
+};
+
+
+function viewDepartments() {
+    // console.log("View Departments")
+    connection.query('SELECT * FROM department', (err, results) => {
+        if (err) throw err;
+        console.table(results)
+    })
+};
+
 function viewEmployees() {
     console.log("View Employees")
     connection.query('SELECT * FROM employee', (err, results) => {
+        if (err) throw err;
+        console.table(results)
+    })
+};
+
+function viewDepartments() {
+    console.log("View Departments")
+    connection.query('SELECT * FROM department', (err, results) => {
+        if (err) throw err;
+        console.table(results)
+    })
+};
+
+function viewRoles() {
+    console.log("View Roles")
+    connection.query('SELECT * FROM role', (err, results) => {
         if (err) throw err;
         console.table(results)
     })
