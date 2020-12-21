81a263646f894037d355baf0a983e4917c6683c9
Sean Melody <seandmelody@gmail.com>
2020-12-20 18:06:51 -0800

    Update Function acceping input

--- app.js
+++ app.js
@@ -344,19 +344,47 @@ function salesCosts() {
     console.log("Sales Costs")
 }
 
+
+//Update Function to update Employee's role
 function update() {
     inquirer
         // Prompt to ask what employee the user would like to update
-        .prompt({
+        .prompt([{
             name: 'updateRole',
             type: 'list',
             message: 'Which employee would you like to update?',
-            choices: ['Emoloyee1', 'Emoloyee2', 'Emoloyee3', 'Emoloyee4'],
-        })
+            choices: ["Matt", "Ashley", "Arya", "Ted"],
+        },
+        {
+            name: 'newRole',
+            type: 'list',
+            message: 'What Role would you like them to have?',
+            choices: ["Developer", "Accountant", "Database Admin", "Traveling Salesman"],
+        }
+        ])
 
+        // Get the answer to the questions
         .then((answer) => {
-            console.log(answer)
-            start()
-        })
+            // console.log(answer)
+            // Connect to the database and insert the info!
+            connection.query(
+                'UPDATE employee SET ? WHERE ?',
+                [
+                    {
+                        role_id: answer.updateRole,
+                    },
+                    {
+                        first_name: answer.newRole,
+                    },
 
-}
+                ],
+                (err, res) => {
+                    if (err) throw err;
+                    //Let the User Know that the employee role has been updated!
+                    console.log(`${answer.updateRole} employee role updated \n`)
+                    // Call the start function to go back to the main menu and restart the questions!
+                    start()
+                }
+            )
+        })
+};
