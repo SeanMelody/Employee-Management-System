d1144abee1765db260828611898dfea727e82c70
Sean Melody <seandmelody@gmail.com>
2020-12-25 15:40:13 -0800

    Delete Functions Working Correctly

--- app.js
+++ app.js
@@ -479,6 +479,7 @@ function updateManager() {
         })
 };
 
+// Delete Function to see what the user would like to delete
 function deleteQuestion() {
     inquirer
         // Prompt to ask what the user would like displayed
@@ -491,7 +492,7 @@ function deleteQuestion() {
 
         // Get the answer to the question
         .then((answer) => {
-            // Based on their answer, either call the deleteDepartments, deleteRoles, deleteEmployee, Functions, or go back to main menu!
+            // Based on their answer, either call the deleteDepartments, deleteRoles, deleteEmployee Functions, or go back to main menu!
             if (answer.deleteDeptRoleEmp === 'Departments') {
                 // Call the delete Departments Function
                 deleteDepartments()
@@ -508,6 +509,7 @@ function deleteQuestion() {
         });
 };
 
+// Delete Department Function to be able to delete Departments
 function deleteDepartments() {
     // Prompt the questions
     inquirer
@@ -518,8 +520,7 @@ function deleteDepartments() {
         })
         // Get the answer to the questions
         .then((answer) => {
-            // Connect to the database and insert the info!
-            // console.log(answer)
+            // Connect to the database and delete the info!
             connection.query(
                 'DELETE FROM department WHERE ?',
                 [
@@ -529,7 +530,7 @@ function deleteDepartments() {
                 ],
                 (err, res) => {
                     if (err) throw err;
-                    //Let the User Know that the department that has been inserted!
+                    //Let the User Know that the department that has been deleted!
                     console.log(`${answer.deleteDept} Department Deleted!\n`);
                     // Call the start function to go back to the main menu and restart the questions!
                     start()
@@ -538,24 +539,92 @@ function deleteDepartments() {
         })
 };
 
-//     console.log('Deleting all strawberry icecream...\n');
-//     connection.query(
-//       'DELETE FROM products WHERE ?',
-//       {
-//         flavor: 'strawberry',
-//       },
-//       (err, res) => {
-//         if (err) throw err;
-//         console.log(`${res.affectedRows} products deleted!\n`);
-//         // Call readProducts AFTER the DELETE completes
-//         readProducts();
-//       }
-//     );
-//   };
-// }
+// Delete Roles Function to be able to delete Roles
 function deleteRoles() {
-    console.log("delete Role")
-}
+    // Prompt the questions
+    inquirer
+        .prompt({
+            name: 'deleteRole',
+            type: 'input',
+            message: "What Role would you like to delete?: ",
+        })
+        // Get the answer to the questions
+        .then((answer) => {
+            // Connect to the database and delete the info!
+            connection.query(
+                'DELETE FROM role WHERE ?',
+                [
+                    {
+                        title: answer.deleteRole
+                    },
+                ],
+                (err, res) => {
+                    if (err) throw err;
+                    //Let the User Know that the Role that has been deleted!
+                    console.log(`${answer.deleteRole} Role Deleted!\n`);
+                    // Call the start function to go back to the main menu and restart the questions!
+                    start()
+                }
+            )
+        })
+};
+
+// Delete Employees Function to be able to delete Employees
 function deleteEmployees() {
-    console.log("delete Employee")
-}
+    // Prompt the questions
+    inquirer
+        .prompt([{
+            name: 'deleteFirstName',
+            type: 'input',
+            message: "What is the Employee's First Name you would like to delete?: ",
+        },
+        {
+            name: 'deleteLastName',
+            type: 'input',
+            message: "What is the Employee's Last Name you would like to delete?: ",
+        }])
+        // Get the answer to the questions
+        .then((answer) => {
+
+            // *** TEST TO MAKE SURE THAT THE PERSON EXISTS IN THE DATABASE ***
+            // connection.query(
+            //     'SELECT first_name, last_name FROM employee',
+            //     [
+            //         {
+            //             first_name: answer.deleteFirstName,
+            //         },
+            //         {
+            //             last_name: answer.deleteLastName,
+            //         },
+            //     ],
+            //     (err, res) => {
+            //         if (err) throw err;
+
+            //         console.log(`${answer.deleteFirstName} ${answer.deleteLastName} Employee Deleted!\n`)
+            //         // Call the start function to go back to the main menu and restart the questions!
+            //         start()
+            //     }
+            // )
+            // *** END TEST ***
+
+            // Connect to the database and delete the info!
+            connection.query(
+                'DELETE FROM employee WHERE ?',
+                [
+                    {
+                        first_name: answer.deleteFirstName,
+                    },
+                    {
+                        last_name: answer.deleteLastName,
+                    },
+                ],
+                (err, res) => {
+                    if (err) throw err;
+                    //Let the User Know that the employee has been deleted!
+                    console.log(`${answer.deleteFirstName} ${answer.deleteLastName} Employee Deleted!\n`);
+                    // Call the start function to go back to the main menu and restart the questions!
+                    start()
+                }
+            )
+        })
+};
